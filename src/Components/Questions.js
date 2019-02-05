07e5d9bb275684bdf30d653aaed9d277b6879504
Marilyn Cotrich <macotrich94@gmail.com>
2019-02-04 20:09:58 -0500

    create questions

---
+++ src/Components/Questions.js
@@ -0,0 +1,24 @@
+const questions = [
+  {
+    question: "What region are you from?",
+    answers: ["Central America", "South America", "Africa", "Asia"]
+  },
+  {
+    question: "Pick Your Preference:",
+    answers: ["Green Tea", "English Breakfast"]
+  },
+  {
+    question: "Pick Your Preference:",
+    answers: ["Pecans", "Almonds"]
+  },
+  {
+    question: "Pick Your Preference:",
+    answers: ["Merlot", "Sauvignon Blanc"]
+  },
+  {
+    question: "Pick Your Preference:",
+    answers: ["Milk Chocolate", "Dark Chocolate"]
+  }
+]
+
+export default questions;
