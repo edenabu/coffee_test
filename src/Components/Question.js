68db490920804d8e83246cf684eb7fd2dc44f1c3
Eden <edenabu777#gmail.com>
2019-02-06 03:48:25 +0200

    questions

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import questions from "./Questions";
 
 class Question extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      question: "",
+      selection: "",
+    }
+  }
+
+  handdleSubmit = (e) => {
+    this.props.addAnswer()
+  }
+  handdleChange = (e) => {
+    this.setState({selection: e.taget.value})
+  }
+  componentDidMount(){
+    let numQuest = Number(this.props.match.params.number)
+    let question = questions[numQuest-1]
+    this.setState({question:question})
+  }
   render() {
-    const question = this.props.question;
+    const question = this.state.question;
 
     return(
-      <ol>
-      {question.question}
-        <br></br>
-        {question.answers}
-      </ol>
+      <div>{question.question}</div>
     )
   }
 }
